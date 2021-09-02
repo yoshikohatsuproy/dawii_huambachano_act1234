2fa8a1c90105e2c55db5ccc79ec96c8aaf3909f9
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-09-01 19:28:31 -0500

    Enviar

---
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -0,0 +1,31 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
+
+@RestController
+@RequestMapping("/rest/alumno")
+public class AlumnoController {
+
+	@Autowired
+	private AlumnoService service;
+
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Alumno>> listaAlumno(){
+		List<Alumno> lista = service.listaAlumno();
+		return ResponseEntity.ok(lista);
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/empresa/service/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Alumno;
+
+public interface AlumnoService {
+
+
+	public List<Alumno> listaAlumno();
+
+}
